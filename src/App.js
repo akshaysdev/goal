92b705d2ceb79ef9673393fe263c91ba53523588
Akshayus27 <akshayus27@gmail.com>
2023-01-09 22:42:44 +0530

    dynamic class for error

--- src/App.js
+++ src/App.js
@@ -25,19 +25,12 @@ const App = () => {
     });
   };
 
-  let content = <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>;
-
-  if (courseGoals.length > 0) {
-    content = <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />;
-  }
-
   return (
     <div>
       <section id="goal-form">
         <CourseInput onAddGoal={addGoalHandler} />
       </section>
       <section id="goals">
-        {content}
         {courseGoals.length > 0 ? (
           <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
         ) : (
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,7 @@ import './CourseInput.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
@@ -12,14 +13,21 @@ const CourseInput = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    } else {
+      setIsValid(true);
+    }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
